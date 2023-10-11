1e308fd0964fd39f40e0c2411580685a8694e22a
YousfiHamza <yousfihamza22@gmail.com>
2023-10-11 10:23:21 +0200

    feat: Clerk - Basic setups

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,9 @@
+const Journal = () => {
+  return (
+    <div>
+      <h1>Welcome To Your Journal !</h1>
+    </div>
+  )
+}
+
+export default Journal
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,29 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Manrope } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
-const inter = Manrope({ subsets: ['latin'] })
+const manrope = Manrope({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Moody.Ai',
   description: 'A Journal with Mood Tracker using AI',
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={manrope.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
