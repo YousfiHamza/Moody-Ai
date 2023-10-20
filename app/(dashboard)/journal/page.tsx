639530a8bbea8b24a4f18fedabc0062a1b4be7c8
YousfiHamza <yousfihamza22@gmail.com>
2023-10-21 00:29:20 +0200

    feat: langchain & openAi initial setups

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -3,10 +3,13 @@ import { getUserByClerkId } from '@/utils/auth';
 import EntryCard from '@/components/modules/EntryCard';
 import { NewEntryCard } from '@/components/modules/NewEntryCard';
 import Link from 'next/link';
+import { analyze } from '@/utils/ai';
 
 const getEntries = async () => {
   const user = await getUserByClerkId();
   const entries = user.entries;
+
+  await analyze('wait a minute ... who are you !?');
   return entries;
 };
 
@@ -16,7 +19,7 @@ const Journal = async () => {
   return (
     <div className="h-full bg-zinc-400/10 p-6">
       <h1 className="mb-8 text-3xl">Welcome To Your Journal !</h1>
-      <div className="gap- grid grid-cols-3 p-8">
+      <div className="grid grid-cols-3 gap-4 p-8">
         <NewEntryCard />
         {entries.map(entry => (
           <Link key={entry.id} href={`/journal/${entry.id}`}>
